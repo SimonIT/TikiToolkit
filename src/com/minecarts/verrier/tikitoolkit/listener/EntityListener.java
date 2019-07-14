2f531fbce63b9d6dfb7c92681ea052194d769747
SimonIT <simonit.orig@gmail.com>
2019-07-14 20:55:32 +0200

    adding intellij and travis configuration and reformat

--- src/com/minecarts/verrier/tikitoolkit/listener/EntityListener.java
+++ src/com/minecarts/verrier/tikitoolkit/listener/EntityListener.java
@@ -1,26 +1,27 @@
 package com.minecarts.verrier.tikitoolkit.listener;
 
-import org.bukkit.event.entity.EntityDeathEvent;
+import com.minecarts.verrier.tikitoolkit.TikiToolkit;
 import org.bukkit.entity.Player;
-
-import com.minecarts.verrier.tikitoolkit.*;
+import org.bukkit.event.entity.EntityDeathEvent;
 
 public class EntityListener extends org.bukkit.event.entity.EntityListener {
 	private TikiToolkit plugin;
-	public EntityListener(TikiToolkit instance)
-	{
+
+	public EntityListener(TikiToolkit instance) {
 		plugin = instance;
 	}
-	
-	public void onEntityDeath(EntityDeathEvent event){
-		if(event.getEntity() instanceof Player){
+
+	public void onEntityDeath(EntityDeathEvent event) {
+		if (event.getEntity() instanceof Player) {
 			Player player = (Player) event.getEntity();
 			//Check to see if their drops are off
-			if(!(plugin.config.getBoolean("admins."+player.getName()+".drop_items", true))){
-			    if(!player.isOp() && plugin.config.getBoolean("admins."+player.getName()+".op_only", false)){ return; }
+			if (!(plugin.config.getBoolean("admins." + player.getName() + ".drop_items", true))) {
+				if (!player.isOp() && plugin.config.getBoolean("admins." + player.getName() + ".op_only", false)) {
+					return;
+				}
 				event.getDrops().clear();
 			}
 		}
 	}
-	
+
 }
